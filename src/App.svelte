749b9632be6cbc9b57812060a9dc1cec104eafc7
Chris Braissant <chrisbraissant@gmail.com>
2020-02-12 08:25:01 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import datas from "./data.js";
+  import Label from "./Label.svelte";
+</script>
+
+<main>
+  <div class="book">
+    {#each datas as data}
+      {#each Array(data.product_quantity) as _, i}
+        <Label {...data} />
+      {/each}
+    {/each}
+  </div>
+</main>
---
+++ src/data.js
@@ -0,0 +1,515 @@
+let products = [
+  {
+    product_code: '2300194903',
+    product_model: '1-9490M',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size M',
+    product_quantity: 1
+  },
+
+  {
+    product_code: '2300294904',
+    product_model: '2-9490L',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300394905',
+    product_model: '2-9490XL',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300394952',
+    product_model: '3-9495S',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size S',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300394953',
+    product_model: '3-9495M',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300394954',
+    product_model: '3-9495L',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300394955',
+    product_model: '3-9495XL',
+    product_label_1: 'Kit Rolock - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300358093',
+    product_model: '3-5809M',
+    product_label_1: 'Kit Rolock - Black drygloves',
+    product_label_2: 'Separated lining - Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300358104',
+    product_model: '3-5810L',
+    product_label_1: 'Kit Rolock - Black drygloves',
+    product_label_2: 'Separated lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300358115',
+    product_model: '3-5811XL',
+    product_label_1: 'Kit Rolock - Black drygloves',
+    product_label_2: 'Separated lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094903',
+    product_model: '9490M',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094904',
+    product_model: '9490L',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094905',
+    product_model: '9490XL',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094952',
+    product_model: '9495S',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094953',
+    product_model: '9495M',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094954',
+    product_model: '9495L',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300094955',
+    product_model: '9495XL',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300058093',
+    product_model: '5809M',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300058094',
+    product_model: '5810L',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300058095',
+    product_model: '5811XL',
+    product_label_1: 'Black drygloves',
+    product_label_2: 'Attached Lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300080083',
+    product_model: '8008',
+    product_label_1: 'Thinsulate inner glove',
+    product_label_2: 'Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300080094',
+    product_model: '8009',
+    product_label_1: 'Thinsulate inner glove',
+    product_label_2: 'Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300080105',
+    product_model: '8010',
+    product_label_1: 'Thinsulate inner glove',
+    product_label_2: 'Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300021683',
+    product_model: '2168 M',
+    product_label_1: 'Kleven inner glove',
+    product_label_2: 'Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300021694',
+    product_model: '2169 L',
+    product_label_1: 'Kleven inner glove',
+    product_label_2: 'Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300021605',
+    product_model: '2160 XL',
+    product_label_1: 'Kleven inner glove',
+    product_label_2: 'Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300081011',
+    product_model: '8101',
+    product_label_1: 'Fleece headband',
+    product_label_2: 'Size S – M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300081010',
+    product_model: '8102',
+    product_label_1: 'Fleece headband',
+    product_label_2: 'Size L – XL ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300095000',
+    product_model: '9500',
+    product_label_1: 'Wrist seal protection',
+    product_label_2: '',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300098010',
+    product_model: '9801',
+    product_label_1: 'Kit Rolock',
+    product_label_2: 'Blue ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300098020',
+    product_model: '9802',
+    product_label_1: 'Kit Rolock',
+    product_label_2: 'Black ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300098030',
+    product_model: '9803',
+    product_label_1: 'Kit Rolock',
+    product_label_2: 'Red ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097070',
+    product_model: '9707L',
+    product_label_1: 'Spare Rolock ring',
+    product_label_2: 'Dryglove side - Left ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097071',
+    product_model: '9707R',
+    product_label_1: 'Spare Rolock ring',
+    product_label_2: 'Dryglove side - Right ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097080',
+    product_model: '9708L',
+    product_label_1: 'Spare Rolock ring',
+    product_label_2: 'Drysuit side - Left ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097081',
+    product_model: '9708R',
+    product_label_1: 'Spare Rolock ring',
+    product_label_2: 'Drysuit side - Right ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300099010',
+    product_model: '9901',
+    product_label_1: 'Spare ring for glove mounting',
+    product_label_2: 'Blue ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300099020',
+    product_model: '9902',
+    product_label_1: 'Spare ring for glove mounting',
+    product_label_2: 'Black ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300099030',
+    product_model: '9903',
+    product_label_1: 'Spare ring for glove mounting',
+    product_label_2: 'Red ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097010',
+    product_model: '9701',
+    product_label_1: 'O-ring set',
+    product_label_2: 'Rolock 1 ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097020',
+    product_model: '9702',
+    product_label_1: 'O-ring set',
+    product_label_2: 'Rolock 2 or 3 ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097030',
+    product_model: '9703',
+    product_label_1: 'Spare O-ring',
+    product_label_2: 'Bayonet lock - From 2006 ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097040',
+    product_model: '9704',
+    product_label_1: 'Spare O-ring',
+    product_label_2: 'Bayonet lock - Up to 2006 ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097050',
+    product_model: '9705',
+    product_label_1: 'Spare O-ring',
+    product_label_2: 'Drysuit mounting ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097060',
+    product_model: '9706',
+    product_label_1: 'Spare O-ring',
+    product_label_2: 'Red or Blue ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300097090',
+    product_model: '9709',
+    product_label_1: 'Spare O-ring',
+    product_label_2: 'Neoprene wrist seal ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300028980',
+    product_model: 'SIP 4',
+    product_label_1: 'Silicone grease',
+    product_label_2: '',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300057530',
+    product_model: 'DAS 70',
+    product_label_1: 'Tube for pressure equalizing',
+    product_label_2: '',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300055020',
+    product_model: 'KF 500',
+    product_label_1: 'Transparent blue box',
+    product_label_2: '',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300090900',
+    product_model: '9090',
+    product_label_1: 'Kit Rolock 90 - Silicone wrist seals',
+    product_label_2: 'No gloves ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300090910',
+    product_model: '9091',
+    product_label_1: 'Kit Rolock 90 - No wrist seals',
+    product_label_2: 'No gloves ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300090920',
+    product_model: '9092',
+    product_label_1: 'PU Ring',
+    product_label_2: 'PU Ring ',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300090930',
+    product_model: '9093',
+    product_label_1: 'Silicone wrist seal',
+    product_label_2: 'Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2300090940',
+    product_model: '9094',
+    product_label_1: 'Silicone wrist seal',
+    product_label_2: 'Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094903',
+    product_model: '90-9490M',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094904',
+    product_model: '90-9490L',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094905',
+    product_model: '90-9490XL',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Attached lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094952',
+    product_model: '90-9495 S',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size S',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094953',
+    product_model: '90-9495M',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094954',
+    product_model: '90-9495L',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309094955',
+    product_model: '90-9495XL',
+    product_label_1: 'Kit Rolock 90 - Blue PVC drygloves',
+    product_label_2: 'Separated lining - Size XL',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309058093',
+    product_model: '90-5809M',
+    product_label_1: 'Kit Rolock 90 - Black latex drygloves',
+    product_label_2: 'Separated lining - Size M',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309058104',
+    product_model: '90-5810L',
+    product_label_1: 'Kit Rolock 90 - Black latex drygloves',
+    product_label_2: 'Separated lining - Size L',
+    product_quantity: 0
+  },
+
+  {
+    product_code: '2309058115',
+    product_model: '90-5811XL',
+    product_label_1: 'Kit Rolock 90 - Black latex drygloves',
+    product_label_2: 'Separated lining - Size XL',
+    product_quantity: 0
+  }
+];
+
+export default products;
